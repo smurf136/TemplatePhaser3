3b6cf63ccf6e80821f2c47d9ca05ce679c201316
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-09-15 13:20:25 +0700

    [Finished]

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -50,30 +50,30 @@ class MainMenu extends Phaser.Scene {
         sign04 = this.add.image(0, 0, 'sign4').setScale(0.25)
 
         Mtext = this.add.text(0, 0, 'Name', {
-            font: '48px Arial',
-            fill: '#000000',
+            fontFamily: 'font1',
+            fill: '#ffffff',
             align: 'center'
-        }).setOrigin(0.5)
+        }).setOrigin(0.5).setFontSize(100)
         Stext01 = this.add.text(0, 0, 'Play', {
-            font: '48px Arial',
-            fill: '#000000',
+            fontFamily: 'font1',
+            fill: '#ffffff',
             align: 'center'
-        }).setOrigin(0.5)
+        }).setOrigin(0.5).setFontSize(60)
         Stext02 = this.add.text(0, 0, 'Setting', {
-            font: '48px Arial',
-            fill: '#0000000',
-            align: 'center'
-        }).setOrigin(0.5)
+            fontFamily: 'font1',
+            fill: '#ffffff',
+            align: 'center',
+        }).setOrigin(0.5).setFontSize(60)
         Stext03 = this.add.text(0, 0, 'Quit', {
-            font: '48px Arial',
-            fill: '#000000',
+            fontFamily: 'font1',
+            fill: '#ffffff',
             align: 'center'
-        }).setOrigin(0.5)
+        }).setOrigin(0.5).setFontSize(60)
 
-        con01 = this.add.container(300, 50, [sign01, Mtext]).setScale(1)
-        con02 = this.add.container(300, 50, [sign02, Stext01]).setScale(1)
-        con03 = this.add.container(300, 50, [sign03, Stext02]).setScale(1)
-        con04 = this.add.container(300, 50, [sign04, Stext03]).setScale(1)
+        con01 = this.add.container(300, -100, [sign01, Mtext]).setScale(1)
+        con02 = this.add.container(300, -100, [sign02, Stext01]).setScale(1)
+        con03 = this.add.container(300, -100, [sign03, Stext02]).setScale(1)
+        con04 = this.add.container(300, -100, [sign04, Stext03]).setScale(1)
         console.log('con01.x: ' + con01.x + ', con01.y: ' + con01.y)
         //Change text to sign0x next commit 
         Stext01.setInteractive();
@@ -90,6 +90,7 @@ class MainMenu extends Phaser.Scene {
 
         _this.tweens.add({
             targets: con01,
+            delay: 1500,
             props: {
                 y: {
                     value: 200,
@@ -99,6 +100,7 @@ class MainMenu extends Phaser.Scene {
         });
         _this.tweens.add({
             targets: con02,
+            delay: 1500,
             props: {
                 y: {
                     value: 400,
@@ -108,6 +110,7 @@ class MainMenu extends Phaser.Scene {
         });
         _this.tweens.add({
             targets: con03,
+            delay: 1500,
             props: {
                 y: {
                     value: 550,
@@ -117,6 +120,7 @@ class MainMenu extends Phaser.Scene {
         });
         _this.tweens.add({
             targets: con04,
+            delay: 1500,
             props: {
                 y: {
                     value: 700,
